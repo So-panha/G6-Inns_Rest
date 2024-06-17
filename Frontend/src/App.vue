23d9626d30ccc1ac7f736861419ff6cc7a92c19f
naydany <dany.nay@student.passerellesnumeriques.org>
2024-06-17 17:57:12 +0700

    create_file_project

---
+++ Frontend/src/App.vue
@@ -0,0 +1,17 @@
+<!-- <script setup lang="ts"></script> -->
+
+<!-- <template>
+  <RouterView />
+</template> -->
+
+
+<template>
+  <router-view />
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+/* Add your styles here */
+</style>
