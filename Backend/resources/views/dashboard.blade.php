9b02dc9a350f4dd1bb34fa1568f8aa60eeee1c07
Sopanha <Sopanha.sin@student.passerellesnumeriques.org>
2024-06-22 15:30:49 +0700

    [feat] edit_profile_admin_and_owner PV-47 PV-48

--- Backend/resources/views/dashboard.blade.php
+++ Backend/resources/views/dashboard.blade.php
@@ -3,15 +3,16 @@
         <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
             <div class="container mx-auto px-6 py-8">
 
-                
-                <h3 class="text-gray-700 text-3xl font-medium">Welcome : {{ auth()->user()->name }}</h3>                
+
+                <h3 class="text-gray-700 text-3xl font-medium">Welcome : {{ auth()->user()->name }}</h3>
 
                 <p>Role : <b>
                     @foreach(auth()->user()->roles as $role)
                         {{ $role->name }}
-                    @endforeach 
+                    @endforeach
                 </b> </p>
-  
+                <h1>Just for testing</h1>
+                
             </div>
         </main>
     </div>
--- Backend/resources/views/layouts/sidebar.blade.php
+++ Backend/resources/views/layouts/sidebar.blade.php
@@ -11,7 +11,7 @@
                     </a>
                 </div>
             </div>
-    
+
             <nav class="mt-10">
                 <a class="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100 {{ Route::currentRouteNamed('admin.dashboard') ? 'active' : '' }} " href="{{ route('admin.dashboard')}}">
                     <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -21,10 +21,10 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
                     </svg>
-    
+
                     <span class="mx-3">Dashboard</span>
                 </a>
-                
+
                 @canany('Role access','Role add','Role edit','Role delete')
                 <a class="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100 {{ Route::currentRouteNamed('admin.roles.index') ? 'active' : '' }}"
                     href="{{ route('admin.roles.index') }}">
@@ -34,7 +34,7 @@
                             d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z">
                         </path>
                     </svg>
-    
+
                     <span class="mx-3">Role</span>
                 </a>
                 @endcanany
@@ -47,22 +47,22 @@
                             d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z">
                         </path>
                     </svg>
-    
+
                     <span class="mx-3">Permission</span>
                 </a>
                 @endcanany
-                
+
                 @canany('User access','User add','User edit','User delete')
                 <a class="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100 {{ Route::currentRouteNamed('admin.users.index') ? 'active' : '' }}"
                     href="{{ route('admin.users.index')}}">
                     <span class="inline-flex justify-center items-center">
                   <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
                 </span>
-    
+
                     <span class="mx-3">User</span>
                 </a>
                 @endcanany
-                
+
                 @canany('Post access','Post add','Post edit','Post delete')
                  <a class="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100 {{ Route::currentRouteNamed('admin.posts.index') ? 'active' : '' }}"
                     href="{{ route('admin.posts.index')}}">
@@ -71,7 +71,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
                         </path>
-                    </svg>    
+                    </svg>
                     <span class="mx-3">Post</span>
                 </a>
                 @endcanany
@@ -90,7 +90,5 @@
                 </a>
                 @endcanany
 
-
-                
             </nav>
         </div>
