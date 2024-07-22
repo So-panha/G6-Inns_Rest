61b151257e30105275d75f0519605ae91a7e89fc
NY.MA <NY.MA@PNC.LAN>
2024-07-22 10:12:12 +0700

    checking code

--- Frontend/src/views/Web/Post/CardService.vue
+++ Frontend/src/views/Web/Post/CardService.vue
@@ -9,40 +9,22 @@
 
       <div class="flex items-center">
         <!-- Search Input -->
-        <div
-          class="max-w-md mx-auto px-4 py-3 mb-5 rounded-md border border-blue-200 flex items-center shadow-md"
-          id="input"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 192.904 192.904"
-            width="20px"
-            class="fill-gray-600 mr-3"
-            style="transform: scaleX(-1)"
-          >
+        <div class="max-w-md mx-auto px-4 py-3 mb-5 rounded-md border border-blue-200 flex items-center shadow-md"
+          id="input">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="20px" class="fill-gray-600 mr-3"
+            style="transform: scaleX(-1)">
             <path
-              d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"
-            ></path>
+              d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
+            </path>
           </svg>
 
-          <input
-            type="text"
-            v-model="searchQuery"
-            placeholder="Search for a house..."
-            @input="searchHouses"
-            @focus="showSuggestions = true"
-            @blur="hideSuggestions"
-            class="form-control"
-          />
+          <input type="text" v-model="searchQuery" placeholder="Search for a house..." @input="searchHouses"
+            @focus="showSuggestions = true" @blur="hideSuggestions" class="form-control" />
 
           <!-- Auto-complete Suggestions -->
           <ul v-if="showSuggestions && suggestions.length" class="suggestions-list">
-            <li
-              v-for="suggestion in suggestions"
-              :key="suggestion"
-              @mousedown.prevent="selectSuggestion(suggestion)"
-              class="suggestion-item"
-            >
+            <li v-for="suggestion in suggestions" :key="suggestion" @mousedown.prevent="selectSuggestion(suggestion)"
+              class="suggestion-item">
               {{ suggestion }}
             </li>
           </ul>
@@ -50,43 +32,25 @@
       </div>
     </div>
     <div class="position-relative">
-      <div
-        id="cards-container"
-        class="d-flex overflow-auto"
-        style="white-space: nowrap; scroll-behavior: smooth"
-      >
-        <div
-          v-for="house in filteredHouses"
-          :key="house.id"
-          class="col-md-4 mb-15 ml-3 d-inline-block"
-          style="width: 400px"
-        >
+      <div id="cards-container" class="d-flex overflow-auto" style="white-space: nowrap; scroll-behavior: smooth">
+        <div v-for="house in filteredHouses" :key="house.id" class="col-md-4 mb-15 ml-3 d-inline-block"
+          style="width: 400px">
           <div class="card h-100 position-relative border-0">
-            <div
-              class="overflow-auto"
-              :id="'scrollContainer-' + house.id"
-              style="white-space: nowrap; scroll-behavior: smooth"
-            >
+            <div class="overflow-auto" :id="'scrollContainer-' + house.id"
+              style="white-space: nowrap; scroll-behavior: smooth">
               <!-- Display images here -->
               <div v-for="image in house.photos" :key="image.url" class="d-inline-block">
-                <img
-                  :src="getImageUrl(image.url)"
-                  class="card-img-top"
-                  style="width: 400px; height: 200px; object-fit: cover"
-                />
+                <img :src="getImageUrl(image.url)" class="card-img-top"
+                  style="width: 370px; height: 330px; font-size: 20px; object-fit: cover" />
               </div>
             </div>
             <!-- Buttons for scrolling images -->
-            <button
-              @click="scrollLeft(house.id)"
-              class="position-absolute top-50 start-0 translate-middle-y btn btn-outline-dark border-0"
-            >
+            <button @click="scrollLeft(house.id)"
+              class="position-absolute top-50 start-0 translate-middle-y btn btn-outline-dark border-0">
               <span class="material-symbols-outlined">chevron_left</span>
             </button>
-            <button
-              @click="scrollRight(house.id)"
-              class="position-absolute top-50 end-0 translate-middle-y btn btn-outline-dark border-0"
-            >
+            <button @click="scrollRight(house.id)"
+              class="position-absolute top-50 end-0 translate-middle-y btn btn-outline-dark border-0">
               <span class="material-symbols-outlined">chevron_right</span>
             </button>
             <div class="card-body">
@@ -100,12 +64,7 @@
               <!-- Guest house address with link -->
               <div class="d-flex align-items-center">
                 <span class="material-symbols-outlined"> home_pin </span>
-                <a
-                  href="#"
-                  class="text-decoration-none"
-                  @click.prevent="showOnMap(house)"
-                  style="font-size: 13px"
-                >
+                <a href="#" class="text-decoration-none" @click.prevent="showOnMap(house)" style="font-size: 13px">
                   <p class="card-text mb-0 ms-2 text-truncate" :title="house.address">
                     {{ house.address }}
                   </p>
@@ -124,11 +83,8 @@
                   <span class="material-symbols-outlined me-2">wifi</span>
                   <span class="material-symbols-outlined">restaurant</span>
                 </div>
-                <router-link
-                  :to="{ name: 'show-room', params: { id: house.id } }"
-                  class="btn btn-info btn-sm showroom"
-                  >Explore Room</router-link
-                >
+                <router-link :to="{ name: 'show-room', params: { id: house.id } }"
+                  class="btn btn-info btn-sm showroom">Explore Room</router-link>
               </div>
             </div>
           </div>
@@ -267,9 +223,9 @@ export default {
       const a =
         Math.sin(dLat / 2) * Math.sin(dLat / 2) +
         Math.cos((lat1 * Math.PI) / 180) *
-          Math.cos((lat2 * Math.PI) / 180) *
-          Math.sin(dLon / 2) *
-          Math.sin(dLon / 2)
+        Math.cos((lat2 * Math.PI) / 180) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2)
       const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
       const distance = R * c // Distance in kilometers
       return distance
@@ -369,5 +325,46 @@ export default {
   padding: 10px;
   border: none;
   width: 450px;
+}
+
+/* Media Queries for Responsiveness */
+@media screen and (min-width: 768px) and (max-width: 1024px) {
+  .container {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+
+  .search-input-container {
+    width: 100%;
+  }
+
+  .shadow-md {
+    height: 50%;
+    width: 50%;
+  }
+
+  #cards-container {
+    height: 80%;
+    width: 140%;
+    justify-items: center;
+  }
+
+  .card-item {
+    width: 70%; /* Increased card size */
+  }
+
+  .card-img-top {
+    height: 200px; /* Decreased image height */
+    width: 500%;
+  }
+
+  .card-body {
+    padding: 20px; /* Slightly increased padding for better visual */
+  }
+  img{
+    width: 100%;
+    height: 100vh;
+  }
 }
 </style>
