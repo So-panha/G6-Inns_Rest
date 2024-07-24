408fd5d9c014d9f48cdb1846e46e488ee046497f
unknown <vanny.mao@student.passerellesnumeriques.org>
2024-07-24 23:18:58 +0700

    [feat] other_geustHouese_and_update_nav_Bar PVG-52

--- Frontend/src/views/Web/guesthousetCardDetail/slideGuestHoustView.vue
+++ Frontend/src/views/Web/guesthousetCardDetail/slideGuestHoustView.vue
@@ -1,37 +1,19 @@
 <template>
   <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
     <div class="carousel-inner">
-      <div class="carousel-item active">
+      <div
+        v-for="(slide, index) in slides"
+        :key="index"
+        :class="['carousel-item', { active: index === 0 }]"
+      >
         <img
-          src="https://www.christiesrealestate.com/blog/wp-content/uploads/2020/10/header-3-1600x550.jpg?is-pending-load=1"
+          :src="slide.imageUrl"
           class="d-block w-100 h-half-screen"
-          alt="Slide 1"
+          :alt="'Slide ' + (index + 1)"
         />
         <div class="carousel-caption d-none d-md-block">
           <h1>GuestHouses</h1>
-          <h5>Find the guesthouses that appeal to you the most</h5>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img
-          src="https://vyn-guesthouse.co.za/wp-content/uploads/2023/05/AMP_3994.jpg"
-          class="d-block w-100 h-half-screen"
-          alt="Slide 2"
-        />
-        <div class="carousel-caption d-none d-md-block">
-          <h1>GuestHouses</h1>
-          <h5>Find the guesthouses that appeal to you the most</h5>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img
-          src="https://b-cdn.springnest.com/media/img/2d/20231023_1719098652bd4.jpg?crop=4000%2C1600%2C0%2C480&width=1500"
-          class="d-block w-100 h-half-screen"
-          alt="Slide 3"
-        />
-        <div class="carousel-caption d-none d-md-block">
-          <h1>GuestHouses</h1>
-          <h5>Find the guesthouses that appeal to you the most</h5>
+          <h5>{{ slide.caption }}</h5>
         </div>
       </div>
     </div>
@@ -58,9 +40,29 @@
 
 <script>
 export default {
+  data() {
+    return {
+      slides: [
+        {
+          imageUrl:
+            'https://www.christiesrealestate.com/blog/wp-content/uploads/2020/10/header-3-1600x550.jpg?is-pending-load=1',
+          caption: 'Find the guesthouses that appeal to you the most'
+        },
+        {
+          imageUrl: 'https://vyn-guesthouse.co.za/wp-content/uploads/2023/05/AMP_3994.jpg',
+          caption: 'Find the guesthouses that appeal to you the most'
+        },
+        {
+          imageUrl:
+            'https://b-cdn.springnest.com/media/img/2d/20231023_1719098652bd4.jpg?crop=4000%2C1600%2C0%2C480&width=1500',
+          caption: 'Find the guesthouses that appeal to you the most'
+        }
+      ]
+    }
+  },
   mounted() {
     // Initialize Bootstrap carousel after component is mounted
-    const carousel = new bootstrap.Carousel(document.getElementById('carouselExampleFade'), {
+    new bootstrap.Carousel(document.getElementById('carouselExampleFade'), {
       interval: 2000 // Adjust interval as needed (in milliseconds)
     })
   }
